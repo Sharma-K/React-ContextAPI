058994853a4ce900c9bcfb34909b10e4fdf11cfd
Keshav Sharma <keshav2743@gmail.com>
2022-11-16 14:32:03 +0530

    Custom context provider component

--- src/components/store/auth-context.js
+++ src/components/store/auth-context.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 //create context takes a default context
 
 //AuthContext is an object that contains the component
 
 const AuthContext = React.createContext({
-    isLoggedIn: false
+    isLoggedIn: false,
+    onLogout: ()=>{}
 });
 
+export const AuthContextProvider = ()=>{
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const logoutHandler = () =>{
+        setIsLoggedIn(false);
+    }
+
+    const loginHandler = () =>{
+        setIsLoggedIn(true);
+    }
+
+    return (<AuthContext.Provider value={{isLoggedIn: isLoggedIn, onLogout: logoutHandler, onLogIn: loginHandler }}>
+        {props.children}
+    </AuthContext.Provider>);
+}
 export default AuthContext;
